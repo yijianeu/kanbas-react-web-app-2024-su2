ded5ca0f0791951243e5a8be649e8561375f9595
yijianeu <152763320+yijianeu@users.noreply.github.com>
2024-07-13 14:45:44 -0700

    Update index.tsx

--- src/Kanbas/index.tsx
+++ src/Kanbas/index.tsx
@@ -1,24 +1,47 @@
-import { Route, Routes } from "react-router";
-import Courses from "./Courses";
-import Dashboard from "./Dashboard";
-import KanbasNavigation from "./Navigation";
+import { AiOutlineDashboard } from "react-icons/ai";
+import { IoCalendarOutline } from "react-icons/io5";
+import { LiaBookSolid } from "react-icons/lia";
+import { FaInbox, FaRegCircleUser } from "react-icons/fa6";
+import { MdClass } from "react-icons/md";
 
-export default function Kanbas() {
-  return (
-    <div id="wd-kanbas">
-      <table border={1} width="100%">
-        <tr>
-          <td valign="top">
-            <KanbasNavigation />
-          </td>
-          <td valign="top">
-            <Routes>
-              <Route path="/Courses/:id/*" element={<Courses />} />
-              <Route path="/Dashboard" element={<Dashboard />} />
-            </Routes>
-          </td>
-        </tr>
-      </table>
-    </div>
-  );
+export default function KanbasNavigation() {
+    return (
+        <div style={{ width: 120 }} id="wd-kanbas-navigation" className="list-group rounded-0 position-fixed bottom-0 top-0 d-none d-md-block bg-black z-2">
+            <a className="list-group-item bg-black text-white border-0 text-center" id="wd-neu-link" href="https://www.northeastern.edu/">
+                <img src="/images/NEU.png" alt="Northeastern logo" width="75px" />
+                Northeastern
+            </a>
+
+            <a className="list-group-item bg-black text-white border-0 text-center" id="wd-account-link" href="#/Kanbas/Account">
+                <FaRegCircleUser className="fs-1 text text-white" /><br />
+                Account
+            </a>
+
+            <a className="list-group-item bg-white text-danger border-0 text-center" id="wd-dashboard-link" href="#/Kanbas/Dashboard">
+                <AiOutlineDashboard className="fs-1 text-danger" /><br />
+                Dashboard
+            </a>
+
+            <a className="list-group-item bg-black text-white border-0 text-center" id="wd-course-link" href="#/Kanbas/Courses">
+                <LiaBookSolid className="fs-1 text-danger" /><br />
+                Courses
+            </a>
+
+            <a className="list-group-item bg-black text-white border-0 text-center" id="wd-calendar-link" href="#/Kanbas/Calendar">
+                <IoCalendarOutline className="fs-1 text-danger" /><br />
+                Calendar
+            </a>
+
+            <a className="list-group-item bg-black text-white border-0 text-center" id="wd-inbox-link" href="#/Kanbas/Inbox">
+                <FaInbox className="fs-1 text-danger" /><br />
+                Inbox
+            </a>
+
+            <a className="list-group-item bg-black text-white border-0 text-center" id="wd-labs-link" href="#/Labs">
+                <MdClass className="fs-1 text-danger" /><br />
+                Labs
+            </a>
+        </div>
+    );
 }
+
