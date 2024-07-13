d591c2e4424afb8a863591e427a9a9ff3eeec599
yijianeu <cao.yij@northeastern.edu>
2024-07-13 14:09:01 -0700

    here is the final version of lab 2

--- src/Kanbas/index.tsx
+++ src/Kanbas/index.tsx
@@ -2,23 +2,18 @@ import { Route, Routes } from "react-router";
 import Courses from "./Courses";
 import Dashboard from "./Dashboard";
 import KanbasNavigation from "./Navigation";
+import "./styles.css";
 
 export default function Kanbas() {
   return (
-    <div id="wd-kanbas">
-      <table border={1} width="100%">
-        <tr>
-          <td valign="top">
-            <KanbasNavigation />
-          </td>
-          <td valign="top">
-            <Routes>
-              <Route path="/Courses/:id/*" element={<Courses />} />
-              <Route path="/Dashboard" element={<Dashboard />} />
-            </Routes>
-          </td>
-        </tr>
-      </table>
-    </div>
+      <div id="wd-kanbas">
+        <KanbasNavigation/>
+        <div className="wd-main-content-offset p-3">
+          <Routes>
+            <Route path="Dashboard" element={<Dashboard/>}/>
+            <Route path="Courses/:cid/*" element={<Courses/>}/>
+          </Routes>
+        </div>
+      </div>
   );
 }
