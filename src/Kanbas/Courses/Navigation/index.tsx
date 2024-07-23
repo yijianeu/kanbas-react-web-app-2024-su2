2092b7060d8faacb5bff095c6eaee2909cce1208
yijianeu <152763320+yijianeu@users.noreply.github.com>
2024-07-22 19:16:00 -0700

    Update index.tsx

--- src/Kanbas/Courses/Navigation/index.tsx
+++ src/Kanbas/Courses/Navigation/index.tsx
@@ -6,19 +6,18 @@ export default function CoursesNavigation() {
    const { pathname } = useLocation();
    const links = ["Home", "Modules", "Piazza", "Zoom", "Assignments", "Quizzes", "Grades"];
    return (
-      <div id="wd-courses-navigation" className="list-group fs-5 rounded-0">
-         {links.map((link) => (
-            <Link
-               key={'/Kanbas/Courses/${cid}/${link}'}
-               to={`/Kanbas/Courses/${cid}/${link}`}
-               className={`list-group-item border border-0 ${
-                  pathname.includes(link) ? "active" : "text-danger"
-               }`}
-            >
-               {link}
-            </Link>
-         ))}
-      </div>
-
+       <div id="wd-courses-navigation" className="list-group fs-5 rounded-0">
+          {links.map((link) => (
+              <Link
+                  key={`/Kanbas/Courses/${cid}/${link}`} // Corrected template string expression
+                  to={`/Kanbas/Courses/${cid}/${link}`}
+                  className={`list-group-item border border-0 ${
+                      pathname.includes(link) ? "active" : "text-danger"
+                  }`}
+              >
+                 {link}
+              </Link>
+          ))}
+       </div>
    );
 }
