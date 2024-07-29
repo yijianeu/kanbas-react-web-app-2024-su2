18d2dfd3a84f8f21c07b801e3d3a0a48f829d370
yijianeu <152763320+yijianeu@users.noreply.github.com>
2024-07-29 16:50:38 -0700

    Delete src/Kanbas directory

--- src/Kanbas/store.ts
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import modulesReducer from "./Courses/Modules/reducer";
-const store = configureStore({
-    reducer: {
-        modulesReducer,
-    },
-});
-export default store;
