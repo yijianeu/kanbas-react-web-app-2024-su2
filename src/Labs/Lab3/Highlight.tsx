786fb1026862c08989f203ecdeb93f92d857c522
yijianeu <152763320+yijianeu@users.noreply.github.com>
2024-07-28 23:47:31 -0700

    Delete src/Labs directory

--- src/Labs/Lab3/Highlight.tsx
+++
@@ -1,8 +0,0 @@
-import { ReactNode } from "react";
-export default function Highlight({ children }: { children: ReactNode }) {
-  return (
-    <span id="wd-highlight" style={{ backgroundColor: "yellow", color: "red" }}>
-      {children}
-    </span>
-  );
-}
