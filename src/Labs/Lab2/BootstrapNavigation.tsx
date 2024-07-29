fd81dfcb30f0d4b54abdf427b0ff3f721920cfd9
yijianeu <152763320+yijianeu@users.noreply.github.com>
2024-07-28 23:48:31 -0700

    Add files via upload

---
+++ src/Labs/Lab2/BootstrapNavigation.tsx
@@ -0,0 +1,34 @@
+export default function BootstrapNavigation() {
+    return (
+        <div id="wd-css-navigating-with-tabs">
+            <h2>Tabs</h2>
+            <ul className="nav nav-tabs">
+                <li className="nav-item">
+                    <a className="nav-link active" href="#active">Active</a>
+                </li>
+                <li className="nav-item">
+                    <a className="nav-link" href="#link1">Link</a>
+                </li>
+                <li className="nav-item">
+                    <a className="nav-link" href="#link2">Link</a>
+                </li>
+                <li className="nav-item">
+                    <a className="nav-link disabled" href="#disabled">Disabled</a>
+                </li>
+            </ul>
+            <div id="wd-css-navigating-with-cards">
+                <h2>Cards</h2>
+                <div className="card" style={{ width: "18rem" }}>
+                    <img src="images/stacked.jpg" className="card-img-top" alt="Stacking Starship" />
+                    <div className="card-body">
+                        <h5 className="card-title">Stacking Starship</h5>
+                        <p className="card-text">
+                            Stacking the most powerful rocket in history. Mars or bust!
+                        </p>
+                        <a href="#boldly-go" className="btn btn-primary">Boldly Go</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
