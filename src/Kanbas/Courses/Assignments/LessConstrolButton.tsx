0c4e2933676c5123074e6d4d84075bf627d52ab5
yijianeu <152763320+yijianeu@users.noreply.github.com>
2024-07-29 17:09:00 -0700

    Delete src/Kanbas directory

--- src/Kanbas/Courses/Assignments/LessConstrolButton.tsx
+++
@@ -1,9 +0,0 @@
-import { IoEllipsisVertical } from "react-icons/io5";
-import GreenCheckmarkNew from "./GreenCheckmarkNew";
-export default function LessControlButtons() {
-  return (
-    <div className="float-end">
-      <GreenCheckmarkNew />
-      <IoEllipsisVertical className="fs-4" />
-    </div>
-);}
