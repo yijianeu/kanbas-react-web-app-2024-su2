59642ea591e61039287ed2e833962f3b7d607e47
yijianeu <152763320+yijianeu@users.noreply.github.com>
2024-08-05 18:53:29 -0700

    Update client.ts

--- src/Kanbas/Courses/Modules/client.ts
+++ src/Kanbas/Courses/Modules/client.ts
@@ -3,8 +3,7 @@ const REMOTE_SERVER = process.env.REACT_APP_REMOTE_SERVER;
 const COURSES_API = `${REMOTE_SERVER}/api/courses`;
 const MODULES_API = `${REMOTE_SERVER}/api/modules`;
 export const updateModule = async (module: any) => {
-    const response = await axios.
-    put(`${MODULES_API}/${module._id}`, module);
+    const response = await axios.put(`${MODULES_API}/${module._id}`, module);
     return response.data;
 };
 
