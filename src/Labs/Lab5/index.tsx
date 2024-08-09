f87043af048b166c8f60514f1f505aae36e9f05f
yijianeu <cao.yij@northeastern.edu>
2024-08-09 16:40:58 -0700

    first

--- src/Labs/Lab5/index.tsx
+++ src/Labs/Lab5/index.tsx
@@ -5,15 +5,13 @@ import WorkingWithArrays from "./WorkingWithArrays";
 import WorkingWithObjectsAsynchronously from "./WorkingWithObjectsAsynchronously";
 import HttpClient from "./HttpClient";
 import WorkingWithArraysAsynchronously from "./WorkingWithArraysAsynchronously";
+const REMOTE_SERVER = process.env.REACT_APP_REMOTE_SERVER;
 export default function Lab5() {
     return (
         <div id="wd-lab5">
             <h1>Lab 5</h1>
             <div className="list-group">
-                <a href="http://localhost:4000/lab5/welcome"
-                   className="list-group-item">
-                    Welcome
-                </a>
+                <a href={`${REMOTE_SERVER}/lab5`}>Hello</a>
                 <h2>Calculator</h2>
                 <EncodingParametersInURLs />
             </div><hr/>
