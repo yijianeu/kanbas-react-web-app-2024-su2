670d6a9aeeb05ada1740298e920670a7f801db11
yijianeu <152763320+yijianeu@users.noreply.github.com>
2024-08-09 17:03:18 -0700

    Update WorkingWithObjects.tsx

--- src/Labs/Lab5/WorkingWithObjects.tsx
+++ src/Labs/Lab5/WorkingWithObjects.tsx
@@ -1,16 +1,17 @@
+const REMOTE_SERVER = process.env.REACT_APP_REMOTE_SERVER;
 export default function WorkingWithObjects() {
     return (
         <div id="wd-working-with-objects">
             <h3>Working With Objects</h3>
             <h4>Retrieving Objects</h4>
             <a id="wd-retrieve-assignments" className="btn btn-primary"
-               href="http://localhost:4000/lab5/assignment">
+               href={'${REMOTE_SERVER}/lab5/assignment'}>
                 Get Assignment
             </a>
             <hr/>
             <h4>Retrieving Properties</h4>
             <a id="wd-retrieve-assignment-title" className="btn btn-primary"
-               href="http://localhost:4000/lab5/assignment/title">
+               href={'${REMOTE_SERVER}/lab5/assignment/title'}>
                 Get Title
             </a>
             <hr/>
